f2914b377c0d49fe3d575067d632bda68619dce3
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2025-08-22 23:41:14 +0200

    Update pretrained.py

--- src/pretrained.py
+++ src/pretrained.py
@@ -1,1 +1,19 @@
+import torch
+import torchvision.models as models
+
+def load_pretrained_model(model_name,num_classes):
+    if model_name == 'vgg16':
+        model = models.vgg16(weights='DEFAULT')#pretrained=True)
+        model.classifier[6] = torch.nn.Linear(in_features=4096, out_features=num_classes)
+    elif model_name == 'vgg19':
+        model = models.vgg19(weights='DEFAULT')#pretrained=True)
+        model.classifier[6] = torch.nn.Linear(in_features=4096, out_features=num_classes)
+    elif model_name == 'mobilenet':
+        model = models.mobilenet_v2(weights='DEFAULT')#pretrained=True)
+        model.classifier[1] = torch.nn.Linear(in_features=1280, out_features=num_classes)
+    else:
+        raise ValueError("Model not supported.")
+    return model
+
+
 
